7b77896eb991c69afa5d300f13ee71e733eaa723
Chanont66 <nont30673@gmail.com>
2025-11-15 06:04:01 +0700

    addGitignore

---
+++ Fetch_data.py
@@ -0,0 +1,57 @@
+# !pip install pandas
+import pandas as pd
+import requests
+
+url = "http://air4thai.pcd.go.th/services/getNewAQI_JSON.php"
+data = requests.get(url).json()
+
+# ข้อมูลอยู่ใน stations ต้องเข้าไปก่อน
+stations = data['stations']
+
+# แปลง json เป็น dataframe
+df = pd.json_normalize(stations)
+
+
+
+
+def clean_data(df):
+    # แยกข้อความด้วย , จะได้ list
+    province_split = df['areaEN'].str.split(', ') 
+
+    # เอาตัวสุดท้ายของ list
+    province_last = province_split.str[-1] 
+
+    # ตัดช่องว่างที่อยู่หน้าและหลังคำ
+    province_clean = province_last.str.strip() 
+
+    # แก้ชื่อจังหวัด
+    province_check = {
+        "Chon buri": "Chon Buri",
+        "Mueang Ang Thong District Ang Thong Province": "Ang Thong",
+        "Pattanee": "Pattani",
+        "Suphun Buri": "Suphan Buri",
+        "Chang Wat Phetchaburi": "Phetchaburi",
+        "Phra Nakhon Si Ayutthaya": "Ayutthaya",
+    }
+
+    # ใช้ replace แก้ชื่อที่เขียนมาแปลกๆ
+    df["province_clean"] = province_clean.replace(province_check)
+
+    # แปลง Text ของ pm เป็นเลข
+    df["pm25_number"] = df["AQILast.PM25.value"].astype(float)
+    
+    # (เช็ตว่าครบ 77 จังหวัดไหม?)
+    # print(df['province_clean'].unique())
+    return df
+
+
+# print(clean_data(df))
+
+
+
+
+
+# ดูแถวแรกว่ามีกี่คอลัมน์
+print(stations[0].keys())
+# (['stationID', 'nameTH', 'nameEN', 'areaTH', 'areaEN', 
+# 'stationType', 'lat', 'long', 'forecast', 'AQILast'])
