bea58c42c1bbc51bbeae61590dd7a32058bc224b
Chanont66 <nont30673@gmail.com>
2025-11-14 11:23:34 +0700

    Test_Json2

--- Data/data.py
+++ Data/data.py
@@ -5,8 +5,21 @@ import json
 with open('D:/ProjectOnGit/Project_BigData/Data/air4thai.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-
+# ข้อมูลอยู่ใน stations
 stations = data['stations']
 
-# ดูแถวแรก
+
+# ดูแถวแรกว่ามีกี่คอลัมน์
 # print(stations[0])
+# print(stations[0].keys())
+# (['stationID', 'nameTH', 'nameEN', 'areaTH', 'areaEN', 'stationType', 'lat', 'long', 'forecast', 'AQILast'])
+
+df_aqi = pd.json_normalize(data["AQILast"])
+
+
+tabel = []
+for i in range(len(stations)):
+    tabel.append(stations[i]+df_aqi[i])
+
+df = pd.DataFrame(tabel)
+print(df)
--- Data/howtoget.py
+++ Data/howtoget.py
@@ -8,3 +8,8 @@ data = {'name': 'John', 'age': 25}
 # print(data.get('name'))  # ได้: John
 # print(data.get('city'))  # ได้: None (ไม่ error!)
 print(data.get('city', 'gg'))  # ได้: 'gg' , (ค่า default)
+
+
+test = [{'date': '2025-11-14', 'time': '10:00', 'PM25':{'color_id': '1', 'aqi': '12', 'value': '7.3'}},]
+
+print(test.get(date))
